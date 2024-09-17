db13c82fb502428366f82eb9b892adc29ae53b1c
NguyennQuangMinh <qminh19502@gmail.com>
2024-09-17 15:11:15 +0700

    cap nhat lai

--- blink/main/blink_example_main.c
+++ blink/main/blink_example_main.c
@@ -1,11 +1,4 @@
-/* Blink Example
 
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
-*/
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
@@ -16,28 +9,25 @@
 
 static const char *TAG = "example";
 
-/* Use project configuration menu (idf.py menuconfig) to choose the GPIO to blink,
-   or you can edit the following line and set a number here.
-*/
-#define BLINK_GPIO CONFIG_BLINK_GPIO
 
-static uint8_t s_led_state = 2;
+#define BLINK_GPIO 2 //CONFIG_BLINK_GPIO
 
-#ifdef CONFIG_BLINK_LED_STRIP
+static uint8_t s_led_state = 0;
 
-static led_strip_handle_t led_strip;
+#ifdef CONFIG_BLINK_LED_RMT
+static led_strip_t *pStrip_a;
 
 static void blink_led(void)
 {
     /* If the addressable LED is enabled */
     if (s_led_state) {
         /* Set the LED pixel using RGB from 0 (0%) to 255 (100%) for each color */
-        led_strip_set_pixel(led_strip, 0, 16, 16, 16);
+        pStrip_a->set_pixel(pStrip_a, 0, 16, 16, 16);
         /* Refresh the strip to send data */
-        led_strip_refresh(led_strip);
+        pStrip_a->refresh(pStrip_a, 100);
     } else {
         /* Set all LED off to clear all pixels */
-        led_strip_clear(led_strip);
+        pStrip_a->clear(pStrip_a, 50);
     }
 }
 
@@ -45,27 +35,9 @@ static void configure_led(void)
 {
     ESP_LOGI(TAG, "Example configured to blink addressable LED!");
     /* LED strip initialization with the GPIO and pixels number*/
-    led_strip_config_t strip_config = {
-        .strip_gpio_num = BLINK_GPIO,
-        .max_leds = 1, // at least one LED on board
-    };
-#if CONFIG_BLINK_LED_STRIP_BACKEND_RMT
-    led_strip_rmt_config_t rmt_config = {
-        .resolution_hz = 10 * 1000 * 1000, // 10MHz
-        .flags.with_dma = false,
-    };
-    ESP_ERROR_CHECK(led_strip_new_rmt_device(&strip_config, &rmt_config, &led_strip));
-#elif CONFIG_BLINK_LED_STRIP_BACKEND_SPI
-    led_strip_spi_config_t spi_config = {
-        .spi_bus = SPI2_HOST,
-        .flags.with_dma = true,
-    };
-    ESP_ERROR_CHECK(led_strip_new_spi_device(&strip_config, &spi_config, &led_strip));
-#else
-#error "unsupported LED strip backend"
-#endif
+    pStrip_a = led_strip_init(CONFIG_BLINK_LED_RMT_CHANNEL, BLINK_GPIO, 1);
     /* Set all LED off to clear all pixels */
-    led_strip_clear(led_strip);
+    pStrip_a->clear(pStrip_a, 50);
 }
 
 #elif CONFIG_BLINK_LED_GPIO
@@ -84,8 +56,6 @@ static void configure_led(void)
     gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
 }
 
-#else
-#error "unsupported LED type"
 #endif
 
 void app_main(void)
---
+++ blink_video_2/main/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+#include "esp_log.h"
+//#include "led_strip.h"
+#include "sdkconfig.h"
+#include "input_iot.h"
+    
+#define BLINK_GPIO 2 //CONFIG_BLINK_GPIO
+
+void input_event_callback(int pin)
+{
+    if(pin == GPIO_NUM_0)
+    {
+        static int x = 0;
+        gpio_set_level(BLINK_GPIO, x);
+        x = 1 - x;
+    }
+}
+
+void app_main(void)
+{
+    gpio_pad_select_gpio(BLINK_GPIO);
+
+    gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
+
+    input_io_create(GPIO_NUM_0, HI_TO_LO);
+
+    input_set_callback(input_event_callback);
+}
